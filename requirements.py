ae52f55a389fe9154629c5b2dc15de96fffd4291
Avirup Dey <54141630+AvirupJU@users.noreply.github.com>
2021-03-30 20:43:40 +0530

    Added requirements.py

---
+++ requirements.py
@@ -0,0 +1,16 @@
+'''
+
+Following must be installed using pip for running the project:
+
+1. Tensorflow 2.1
+
+2. Numpy 1.19
+
+3. OpenCV 3.4.4.19
+(Only this version seems to work properly, without throwing errors)
+
+4. keras2onnx
+
+5. onnxruntime
+
+'''
